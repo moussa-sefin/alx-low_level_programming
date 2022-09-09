53b4dbe19148a8d4c995b963a36d549dafbbf918
moussa <yahmoussa5@gmail.com>
2022-09-08 22:09:35 -0400

    hello word project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put fuction
+*
+*Description using the main fuction
+*this program prints Programming is like building a multilingual puzzle
+* return:0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0
+}
+
