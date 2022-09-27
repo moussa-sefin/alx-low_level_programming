aa42cb8eb8a16370882df2ea523473ca2230d801
moussa <yahmoussa5@gmail.com>
2022-09-27 01:51:08 -0400

    task0 review

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,29 +1,18 @@
-/*                                                                                                                                   
- * File: 0-memset.c                                                                                                                  
- * Auth: not                                                                                                                         
- */                                                                                                                                  
-                                                                                                                                     
-#include "main.h"                                                                                                                    
-                                                                                                                                     
-/**                                                                                                                                  
- * _memset - Fills the first n bytes of the memory area                                                                              
- *          pointed to by @s with the constant byte @c.                                                                              
- * @s: A pointer to the memory area to be filled.                                                                                    
- * @c: The character to fill the memory area with.                                                                                   
- * @n: The number of bytes to be filled.                                                                                             
- * description _memset: over there                                                                                                   
- *                                                                                                                                   
- * Return: A pointer to the filled memory area @s.                                                                                   
- *                                                                                                                                   
- */                                                                                                                                  
-void *_memset(void *s, int c, size_t n)                                                                                              
-{                                                                                                                                    
-        unsigned int index;                                                                                                          
-        unsigned char *memory = s, value = c;                                                                                        
-                                                                                                                                     
-        for (index = 0; index < n; index++)                                                                                          
-                memory[index] = value;                                                                                               
-                                                                                                                                     
-        return (memory);                                                                                                             
+#include "main.h"
+
+/**
+  * _memset - fills memory with a constant byte
+  * @s: memory area
+  * @b: the byte
+  * @n: # of bytes to fill
+  * Return: pointer to s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n-- > 0)
+	*ptr++ = b;
+	return (s);
 }
 
