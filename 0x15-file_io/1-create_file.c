e7c9c1ca3b1954ad73a12581595feb441ed06a34
moussa <yahmoussa5@gmail.com>
2022-10-31 16:58:53 -0400

    task1 fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,32 +1,38 @@
 #include "main.h"
+
 /**
- * create_file - creates a file and adds permission to it
- * @filename: The name of the file to be created
- * @text_content: content to be placed in the created file
- * Return: 1 if successfull and -1 on err.
+ * create_file - creates a file
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 if it success. -1 if it fails.
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fild = 0, chk = 0, i = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
 	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
 		return (-1);
 
 	if (!text_content)
 		text_content = "";
 
-	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
-	if (fild < 0)
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
-	while (text_content[i])
-		i++;
-	chk = write(fild, text_content, i);
-	if (chk < 0)
-		return (-1);
+	close(fd);
 
-	close(fild);
 	return (1);
 }
-
-
